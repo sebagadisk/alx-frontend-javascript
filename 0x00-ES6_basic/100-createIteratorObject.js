a3bcd457fcb2e24c5b9359df6317db980ce34b48
sebagadisk <sebexk3@gmail.com>
2022-04-06 17:45:19 +0000

    update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,12 @@
 export default function createIteratorObject(report) {
-  return (function* _() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
-        yield employee;
-      }
+  const workers = [];
+  /* eslint-disable no-unused-vars */
+  for (const [department, employees] of Object.entries(report.allEmployees)) {
+    for (const employe of employees) {
+      workers.push(employe);
     }
-  }());
+  }
+  /* eslint-enable no-unused-vars */
+
+  return workers;
 }
