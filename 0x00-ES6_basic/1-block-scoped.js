9aa350c7828804b3ed0de7ce58768b25a1c38dbf
sebagadisk <sebexk3@gmail.com>
2022-04-06 17:41:27 +0000

    update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,11 @@
-export default function taskBlock (trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line
     const task = true;
+    // eslint-disable-next-line
     const task2 = false;
   }
 
